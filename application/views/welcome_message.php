012ce1a28988b8c9999a633b31e6eb01d1a6574b
qorieinsyira <qorie.irra.fitria@gmail.com>
2019-09-09 14:25:04 +0700

    upload kedua

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,13 @@
-"Hello World :)"
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Page Title</title>
+    </head>
+<body>
+
+    <h1><b>This is me</b></h1>
+    <h2>Y_Karina is here.</h2>
+
+
+</body>
+</html>
